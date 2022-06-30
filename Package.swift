083af65db915081c8a72487235125f4f55ca5dbc
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2022-06-30 03:46:52 -0700

    Fix platforms.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,10 @@ import PackageDescription
 
 let package = Package(
   name: "WrkstrmColor",
-  platforms: [.iOS(.v13), .macOS(.v11)],
+  platforms: [
+    .iOS(.v13),
+    .macOS(.v12)
+  ],
   // Products define the executables and libraries produced by a package, and make them visible
   // to other packages.
   products: [
