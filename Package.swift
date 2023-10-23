2cf247501b36a5b39d12b4fff291fb4bcfce6358
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-10-22 17:11:17 -0700

    Add a new action: - Update wrkstrm-log-build.yml - Update relevant Package.swift - Drop macOS version to 12, - Raise swift to 5.9 - Update to use latest version of swift actions. (+3 squashed commits)

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   name: "WrkstrmColor",
   platforms: [
     .iOS(.v13),
-    .macOS(.v13),
+    .macOS(.v12),
   ],
   // Products define the executables and libraries produced by a package, and make them visible
   // to other packages.
