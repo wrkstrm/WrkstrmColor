1f226b14830193d6f94c3d5d899ef665273e95c7
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-11-10 14:33:54 -0800

    Update HSLuvTests.swift

--- Tests/WrkstrmColorTests/HSLuvTests.swift
+++ Tests/WrkstrmColorTests/HSLuvTests.swift
@@ -6,6 +6,12 @@ import XCTest
 // TODO: Add HPLuv tests
 
 class HSLuvTests: XCTestCase {
+  static var allTests = [
+    ("testConversionConsistency", testConversionConsistency),
+    ("testRgbRangeTolerance", testRgbRangeTolerance),
+    ("testSnapshot", testSnapshot),
+  ]
+  
   let rgbRangeTolerance = 0.000_000_001
   let snapshotTolerance = 0.000_000_001
 
