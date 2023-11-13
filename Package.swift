9be51e522a6ddc662918f40ec6d89f66a9ea63b8
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2023-11-12 16:54:20 -0800

    Update to recursive checkpoints.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 extension SwiftSetting {
   static let profile: SwiftSetting = .unsafeFlags([
     "-Xfrontend",
-    "-warn-long-expression-type-checking=10",
+    "-warn-long-expression-type-checking=5",
   ])
 }
 
